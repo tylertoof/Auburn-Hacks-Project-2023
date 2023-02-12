ad542b4005c81fe821767f29c230a0af620d955a
Tyler Teufel <tdt0025@auburn.edu>
2023-02-11 19:59:34 -0600

    First commit

---
+++ WeeklyWrapped/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WeeklyWrapped.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
